7bc5cb297d51105a9ba2408aafbf272a42a4caef
Lewis Liu <develop@lewisl.net>
2016-01-13 22:29:50 -0500

    Removing stateless components to fix hot-reload

--- app/containers/app.jsx
+++ app/containers/app.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions/actions';
 
-let App = ({ 
-	dispatch
-}) => (
-	<div className="container" style={({
-		marginTop: '1em'
-	})}>
-		<h1>Snake Swarm</h1>
+// using stateless components isn't supported with hot reload yet
+// let App = ({ 
+// 	dispatch
+// }) => (
+// 	<div className="container" style={({
+// 		marginTop: '1em'
+// 	})}>
+// 		<h1>Snake Swarm</h1>
     
-	</div>
-);
+// 	</div>
+// );
+
+class App extends Component {
+  render() {
+    const { dispatch } = this.props;
+    return (
+      <div className="container" style={({
+        marginTop: '1em'
+      })}>
+        <h1>Snake Swarm</h1>
+        
+      </div>
+    );
+  }
+}
 
 export default connect(state => ({ 
 }))(App);
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,15 +1,12 @@
 'use strict';
-// Hot reloading doesn't work right now due to the fact that 
-// React v0.14's stateless components [1] aren't supported yet [2]
-// [1] https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
-// [2] https://github.com/gaearon/babel-plugin-react-transform/issues/57
+
 let path = require('path');
 let webpack = require('webpack');
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     './app/app'
